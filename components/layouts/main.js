c199846104e4b9e409f2209079b957a5f25913c8
starigg <starkovivan3@gmail.com>
2022-11-23 13:58:03 +0800

    meta tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import Head from 'next/head'
-import { Box, Container } from '@chakra-ui/react'
+import {Box, Container} from '@chakra-ui/react'
 import Navbar from '../navbar'
 import VoxelDog from '../voxel-dog'
 import NoSsr from '../no-ssr'
 
-const Main = ({ children, router }) => {
-  return (
-    <Box as="main" pb={8}>
-      <Head>
-        <meta name="viewport" content="width-device-width, initial-scale=1" />
-        <title>Ivan Starkov - Homepage</title>
-      </Head>
+const Main = ({children, router}) => {
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width-device-width, initial-scale=1"/>
+                <meta
+                    name="description"
+                    content="Starkov Ivan Website"
+                />
+                <meta name="google-site-verification" content="L3A06pEhVNAFLonseNimA_xYExquRnbAeu3oqGkiORM"/>
+                <title>Ivan Starkov - Homepage</title>
+            </Head>
 
-      <Navbar path={router.asPath} />
+            <Navbar path={router.asPath}/>
 
-      <Container maxW="container.md" pt={14}>
-        <NoSsr>
-          <VoxelDog />
-        </NoSsr>
-        {children}
-      </Container>
-    </Box>
-  )
+            <Container maxW="container.md" pt={14}>
+                <NoSsr>
+                    <VoxelDog/>
+                </NoSsr>
+                {children}
+            </Container>
+        </Box>
+    )
 }
 
 export default Main;
