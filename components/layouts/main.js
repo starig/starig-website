2430ab9397799749fedf10973b3a186d5fcb56b0
starig <starkovivan3@gmail.com>
2023-05-31 15:49:27 +0800

    before changes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -13,7 +13,7 @@ const Main = ({children, router}) => {
                 <meta name='title' content='Ivan starig Starkov'/>
                 <meta name="description"
                       content="starig personal website, frontend development, CV, mobile development, portfolio"/>
-                <meta name='keywords' content={'starig, STARIG, Starkov Ivan, Старков Иван, Старков, frontend'}/>
+                <meta name='keywords' content={'starig, STARIG, Starkov Ivan, Старков Иван, Старков, frontend, flutter developer, flutter mobile'}/>
                 <meta name="google-site-verification" content="L3A06pEhVNAFLonseNimA_xYExquRnbAeu3oqGkiORM"/>
                 <title>Ivan Starkov - Homepage</title>
             </Head>
