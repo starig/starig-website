d6d55b206652a1af75604eba5595eab799952af2
starig <starkovivan3@gmail.com>
2023-11-14 12:14:59 +0300

    keywords changes

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,10 +10,10 @@ const Main = ({children, router}) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width-device-width, initial-scale=1"/>
-                <meta name='title' content='Ivan starig Starkov'/>
+                <meta name='title' content='Ivan Starkov'/>
                 <meta name="description"
-                      content="starig personal website, frontend development, CV, mobile development, portfolio"/>
-                <meta name='keywords' content={'starig, STARIG, Starkov Ivan, Старков Иван, Старков, frontend, flutter developer, flutter mobile'}/>
+                      content="Иван Старков - Гений, миллиардер, плейбой, филантроп. Один из лучших программистов мира согласно исследованиям за 2023 год."/>
+                <meta name='keywords' content={'starig, STARIG, Starkov Ivan, Старков Иван, Старков, frontend, flutter developer, flutter mobile, Иван Старков, иван старков, старков иван'}/>
                 <meta name="google-site-verification" content="L3A06pEhVNAFLonseNimA_xYExquRnbAeu3oqGkiORM"/>
                 <title>Ivan Starkov - Homepage</title>
             </Head>
