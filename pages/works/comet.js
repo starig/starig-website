931a4e9aeb46c919c2c54ed2b9490751182dc8a7
starig <starkovivan3@gmail.com>
2024-12-23 16:24:04 +0300

    page

---
+++ pages/works/comet.js
@@ -0,0 +1,87 @@
+import {Badge, Box, Button, Container, Heading, List, ListItem, useColorModeValue} from "@chakra-ui/react";
+import {ChevronLeftIcon, ExternalLinkIcon} from "@chakra-ui/icons";
+import {Meta, Title, WorkImage} from "../../components/work";
+import Layout from "../../components/layouts/article";
+import WorkLink from "../../components/work-link";
+import {BioSkill} from "../../components/bio";
+import {Stack} from "../../components/projectStack";
+import NextLink from "next/link";
+import Paragraph from "../../components/paragraph";
+import Section from "../../components/section";
+import worksTranslation from './../../public/locale/works.json';
+import {useSelector} from "react-redux";
+
+const Work = () => {
+    const currentLocale = useSelector((state) => state.language.locale);
+
+    return (
+        <Layout title={'Comet'}>
+            <Container>
+                <Title>
+                    Digital UB <Badge>2023</Badge>
+                </Title>
+                <>
+                    <WorkImage src={'/images/smartub.png'} alt={'Digital UB'}/>
+                    <Heading as={'h4'} fontSize={24}>{worksTranslation[currentLocale].tech_stack}</Heading>
+                    <Stack>
+                        <li>
+                            <BioSkill>Flutter</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>Dart</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>BloC (Cubit)</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>dio</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>Freezed</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>Clean Architecture</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>Rest API</BioSkill>
+                        </li>
+                        <li>
+                            <BioSkill>Swagger</BioSkill>
+                        </li>
+                    </Stack>
+                </>
+                <Section>
+                    <Heading as={'h4'} fontSize={24}>{worksTranslation[currentLocale].about_project}</Heading>
+                    <Paragraph>
+                        <b>Digital UB</b> — {worksTranslation[currentLocale].digitalUb.description}
+                    </Paragraph>
+                </Section>
+                <List>
+                    <ListItem>
+                        <Meta>App Store</Meta>
+                        <WorkLink href={'https://apps.apple.com/lv/app/digital-ub/id6443485509'} target="_blank">
+                            App Store <ExternalLinkIcon mx={'2px'}/>
+                        </WorkLink>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Google Play Store</Meta>
+                        <WorkLink href={'https://play.google.com/store/apps/details?id=com.ictgroup.smartub'} target="_blank">
+                            Google Play Store <ExternalLinkIcon mx={'2px'}/>
+                        </WorkLink>
+                    </ListItem>
+                </List>
+                <Box align={'center'} my={4}>
+                    <NextLink href={'/works'}>
+                        <Button leftIcon={<ChevronLeftIcon/>}
+                                bg={useColorModeValue('#F56565', '#E53E3E')}
+                                _hover={{backgroundColor: useColorModeValue('#f53737', '#414141')}}>
+                            {worksTranslation[currentLocale].goBack}
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
